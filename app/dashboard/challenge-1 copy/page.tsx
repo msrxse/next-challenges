ded2fdc2b64fbb37589140312f0b97bc069019e7
msr <msrxse@gmail.com>
2025-04-08 15:29:59 +0100

    Add copy of challenge folder

---
+++ app/dashboard/challenge-1 copy/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Page() {
+  return (
+    <main className="bg-gray-100 h-full flex justify-center items-center">
+      Challenge-1
+    </main>
+  );
+}
+
+export default Page;
